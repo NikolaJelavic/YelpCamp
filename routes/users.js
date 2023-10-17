e8f6e9d1d17b59cfb5cd29f323adb7f40f7855ef
NikolaJelavic <jelavicnikola@gmail.com>
2023-10-18 00:57:07 +0200

    login routes

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
@@ -23,4 +24,21 @@ router.post('/register', catchAsync(async (req, res, next) => {
     }
 }));
 
-module.exports=router;
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+})
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+})
+
+module.exports = router;
